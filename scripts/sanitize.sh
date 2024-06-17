18edfda6af4ee15b3580e26e405b5e48b84a9814
brycebenton <bryce.benton@austintexas.gov>
2024-06-17 14:17:16 -0500

    sanitizer refinements

--- scripts/sanitize.sh
+++ scripts/sanitize.sh
@@ -1,2 +1,7 @@
 ddev drush sql-cli < sanitize.sql
 ddev drush user:password User1 '111'
+ddev drush cex -y
+cp config/* ../html/sites/default/files/sync
+ddev drush cim -y
+ddev drush cr
+ddev launch
